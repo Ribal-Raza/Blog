122273d4378cf6c08bad5235cfa38e144567981c
Ribal-Raza <ribalraza1@gmail.com>
2024-11-23 20:15:55 +0500

    Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
-  return <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64"></div>;
+  return (
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+      {/* NAVBAR */}
+      <Navbar />
+      {/* BREADCRUMB */}
+      {/* INTRODUCTION */}
+      {/* POSTLIST */}
+    </div>
+  );
 };
 
 export default App;
